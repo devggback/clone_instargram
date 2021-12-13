3f604adbb091557e128fff652e44024c93618914
devggback <kdongha1955@gmail.com>
2021-12-13 14:16:00 +0900

    springboot 초기 설정

---
+++ src/main/java/clone/instar/clone_instar/CloneInstarApplication.java
@@ -0,0 +1,13 @@
+package clone.instar.clone_instar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneInstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneInstarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clone/instar/clone_instar/CloneInstarApplicationTests.java
@@ -0,0 +1,13 @@
+package clone.instar.clone_instar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneInstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
